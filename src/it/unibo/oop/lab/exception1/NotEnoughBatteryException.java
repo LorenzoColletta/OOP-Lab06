9cde423616e2d0591e32816886e19d67bd71a4e5
Lorenzo Colletta <id.boh06@gmail.com>
2021-11-11 16:40:01 +0100

    NotEnoughBatteryException completed

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -6,11 +6,18 @@ public class NotEnoughBatteryException extends IllegalStateException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public NotEnoughBatteryException() {
+    private final int x;
+    private final int y;
+    
+	public NotEnoughBatteryException(final int initX, final int initY) {
 		super();
+		this.x = initX;
+		this.y = initY;
 	}
 	
+	public String toString() {
+        return "Can not move to position(" + this.x + "," + this.y + "). Not enough battery.";
+	}
 	
 	public String getMessage() {
 		return "Not Enough Battery left.";
