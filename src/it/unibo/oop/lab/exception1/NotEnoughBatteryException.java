1576d5a04641cc8324837c8f5314962833ceb994
Lorenzo Colletta <id.boh06@gmail.com>
2021-11-11 16:34:56 +0100

    NotEnoughBatteryException created

---
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -0,0 +1,18 @@
+package it.unibo.oop.lab.exception1;
+
+public class NotEnoughBatteryException extends IllegalStateException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotEnoughBatteryException() {
+		super();
+	}
+	
+	
+	public String getMessage() {
+		return "Not Enough Battery left.";
+	}
+}
